90e2f3aee7376ec48d26691e140fbdf0141fec62
Raazk10 <rakioraag10@gmail.com>
2023-05-28 00:41:27 +0200

    Import documents

--- _studio/schemas/schemas.js
+++ _studio/schemas/schemas.js
@@ -1,3 +1,7 @@
+import category from "./documents/category.js";
+import contact from "./documents/contact.js";
 import product from "./documents/product.js";
+import settings from "./documents/settings.js";
+import tag from "./documents/tag.js";
 
-export default [product];
+export default [product, category, contact, settings, tag];
