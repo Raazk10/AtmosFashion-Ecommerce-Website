a0f1fd80168ed743ed276cbd7085dbf0946bfd1f
Raazk10 <rakioraag10@gmail.com>
2023-05-29 17:00:38 +0200

    Add readurl util

---
+++ _app/js/util/readUrl.js
@@ -0,0 +1,29 @@
+/**
+ * Reads the current URL and returns a specific value based on the URL.
+ *
+ * @returns {string|undefined} The specific value based on the URL, or `undefined` if no matching value is found.
+ */
+
+export function readUrl() {
+  const allUrl = window.location.href;
+
+  // Check if the URL contains "contact"
+  if (allUrl.includes("contact")) {
+    return "contact-us";
+  }
+
+  // Check if the URL contains "product"
+  if (allUrl.includes("product")) {
+    const urlString = window.location.search;
+    console.log(urlString);
+
+    // Check if there is a query string in the URL
+    if (urlString) {
+      return urlString.slice(1); // Slice removes the "?" from the URL
+    }
+  }
+
+  return undefined;
+}
+
+// This code is provided by Carlo during veiledning time
