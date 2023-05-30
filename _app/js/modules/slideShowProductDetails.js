ecb023b47c63742fbc2446b37e2af0816436f93b
Raazk10 <rakioraag10@gmail.com>
2023-05-30 15:00:26 +0200

    Add touch event for slideshow in mobile devices

--- _app/js/modules/slideShowProductDetails.js
+++ _app/js/modules/slideShowProductDetails.js
@@ -1,7 +1,10 @@
 export default function slideShowProductImage() {
   let currentSlideIndex = 0;
+  let touchStartX = 0;
+  let touchEndX = 0;
 
   const slideshow = document.querySelector(".slideShowProduct");
+
   const buttonPrevious = document.querySelector(
     ".slideShowProduct__button-previous"
   );
@@ -11,6 +14,30 @@ export default function slideShowProductImage() {
     const slideShowSlides = slideshow.querySelectorAll(
       ".slideShowProduct__slide"
     );
+    slideShowSlides.forEach((slide) => {
+      slide.addEventListener("touchstart", handleTouchStart);
+      slide.addEventListener("touchmove", handleTouchMove);
+      slide.addEventListener("touchend", handleTouchEnd);
+    });
+    function handleTouchStart(event) {
+      touchStartX = event.touches[0].clientX;
+    }
+
+    function handleTouchMove(event) {
+      touchEndX = event.touches[0].clientX;
+    }
+
+    function handleTouchEnd() {
+      if (touchStartX - touchEndX > 50) {
+        // Swiped to the left, go to the next slide
+        increaseCurrentSlideIndex();
+        renderHtml();
+      } else if (touchEndX - touchStartX > 50) {
+        // Swiped to the right, go to the previous slide
+        decreaseCurrentSlideIndex();
+        renderHtml();
+      }
+    }
 
     buttonPrevious.addEventListener("click", handleButtonPreviousClick);
     buttonNext.addEventListener("click", handleButtonNextClick);
